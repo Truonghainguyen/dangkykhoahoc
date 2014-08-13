31248afc8ff7c77cfa9de09e3252fd351f9e26e4
My name <you@example.com>
2014-08-13 20:00:36 +0700

    Ping Lab

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dangkykhoahoc::Application.config.secret_token = '08de562fe9f4c11759b06d12b53622c7dd09bc8ceb02b1050d99d7c8a6811287bd5d46f273770464a0c4c6373bd4b95814ade72ad662a766026ff68740fed355'
